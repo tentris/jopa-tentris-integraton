41e7dbec614cef2c6b4ba9aae4bae65ac8d8b1b9
SandroMiudo <sandro@tentris.io>
2026-08-28 16:08:10 +0200

    add configurable endpoints for tentris

---
+++ ontodriver-tentris/src/main/jave/cz/cvut/kbss/ontodriver/tentris/config/TentrisConfigParam.java
@@ -0,0 +1,23 @@
+package cz.cvut.kbss.ontodriver.tentris.config;
+
+import cz.cvut.kbss.ontodriver.config.ConfigurationParameter;
+import cz.cvut.kbss.ontodriver.config.OntoDriverProperties;
+
+public enum TentrisConfigParam implements ConfigurationParameter {
+
+    USERNAME(OntoDriverProperties.DATA_SOURCE_USERNAME),
+    PASSWORD(OntoDriverProperties.DATA_SOURCE_PASSWORD),
+    QUERY_ENDPOINT(TentrisProperties.QUERY_ENDPOINT),
+    UPDATE_ENDPOINT(TentrisProperties.UPDATE_ENDPOINT);
+
+    private final String name;
+
+    TentrisConfigParam(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+} 
---
+++ ontodriver-tentris/src/main/jave/cz/cvut/kbss/ontodriver/tentris/config/TentrisProperties.java
@@ -0,0 +1,9 @@
+package cz.cvut.kbss.ontodriver.tentris.config;
+
+public final class TentrisProperties {
+
+    public static final String UPDATE_ENDPOINT = "cz.cvut.kbss.ontodriver.tentris.update-endpoint";
+
+    public static final String QUERY_ENDPOINT = "cz.cvut.kbss.ontodriver.tentris.query-endpoint";
+
+}
