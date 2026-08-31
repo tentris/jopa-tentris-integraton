df6521462579aa8ae8e010674aad7e6d83adeb02
SandroMiudo <sandro@tentris.io>
2026-08-31 17:06:31 +0200

    allow setting custom configurations to align with tentris configurations

---
+++ ontodriver-tentris/src/main/java/cz/cvut/kbss/ontodriver/tentris/config/TentrisProperties.java
@@ -0,0 +1,16 @@
+package cz.cvut.kbss.ontodriver.tentris.config;
+
+public final class TentrisProperties {
+
+    public static final String UPDATE_ENDPOINT = "cz.cvut.kbss.ontodriver.tentris.update-endpoint";
+
+    public static final String QUERY_ENDPOINT = "cz.cvut.kbss.ontodriver.tentris.query-endpoint";
+
+    public static final String HOST = "cz.cvut.kbss.ontodriver.tentris.host";
+
+    public static final String PORT = "cz.cvut.kbss.ontodriver.tentris.port";
+
+    public static final String USERNAME = "cz.cvut.jopa.dataSource.tentris.username";
+
+    public static final String PASSWORD = "cz.cvut.jopa.dataSource.tentris.password";
+}
